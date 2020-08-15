9c58d363d0bb0841e7da402b64c389ed97120f74
IGkougkousis <iraklisgougousis@protonmail.com>
2020-08-15 11:26:14 +0200

    Small refactoring to grpc-server

--- grpc-server/server.py
+++ grpc-server/server.py
@@ -3,40 +3,63 @@ import logging
 import os
 import grpc
 
+from typing import List, Tuple, Dict, NewType
 from concurrent import futures
 from fastnumbers import fast_float
 from measurement_pb2 import Measurement, MeterUsage
 from measurement_pb2_grpc import add_MeterUsageServiceServicer_to_server, MeterUsageService
 
 
-def get_csv_meter_usage():
+def get_raw_csv_data(header: bool = False) -> List[Tuple[str, str]]:
+    """Gets data from the csv as a list of tuples.
+
+    Parameters:
+    - header: bool
+        Wheter to include the csv header row.
+    """
     filename = 'data/meterusage.csv'
     with open(filename, newline='') as f:
         reader = csv.reader(f)
+        data = [(x[0], x[1]) for x in reader]
 
-        # using fastnumbers.fast_float to parse floats from string
-        # on failure assign 0.0
-        def to_float(x): return fast_float(x, default=0.0, nan=0.0, inf=0.0)
-
-        # map csv data to dict
-        data = [{'time': x[0], 'meterusage': to_float(x[1])} for x in reader]
-
-        # skip header row
-        data = data[1:]
+        if(False == header):
+            data = data[1:]
 
         return data
 
 
+time = NewType('time', str)
+measurement = NewType('measurement', float)
+
+
+def parse_raw_data(raw_data: List[Tuple[str, str]]) -> List[Tuple[time, measurement]]:
+    """Parses the raw csv data. 
+    'time' is left unchanged. 'measurement' is converted to float.
+    Invadid floats default to 0.0
+
+    Parameters:
+    - raw_data: List[Tuple[str,str]]
+        The raw csv data. 
+    """
+
+    # using fastnumbers.fast_float to parse floats from string
+    # on failure assign 0.0
+    def to_float(x): return fast_float(x, default=0.0, nan=0.0, inf=0.0)
+
+    return [(x[0], to_float(x[1])) for x in raw_data]
+
+
 class MeterUsageFromCSV(MeterUsageService):
 
-    def GetMeterUsage(self, request, context):
-        csv_data = get_csv_meter_usage()
+    def GetMeterUsage(self, request, context) -> Measurement:
+        raw = get_raw_csv_data()
+        parsed_data = parse_raw_data(raw)
 
-        # convert to grpc class, that is what clients expect
-        converted = [Measurement(time=x['time'], meterusage=x['meterusage'])
-                     for x in csv_data]
+        # convert to grpc classes, that is what clients expect
+        measurements = [Measurement(time=x[0], meterusage=x[1])
+                        for x in parsed_data]
 
-        meter_usage = MeterUsage(measurements=converted)
+        meter_usage = MeterUsage(measurements=measurements)
         return meter_usage
 
 
